2b3cdab0a06486b1c3fb9da0446cea66811de8be
itc-s24020 <s24020@std.it-college.ac.jp>
2024-12-10 15:58:52 +0900

    現在時刻を表示してみよう

---
+++ CH05/CH05-04/script.js
@@ -0,0 +1,8 @@
+var time_el = document.getElementById('time_el');
+
+var date = new Date();
+var hour = date.getHours();
+var min = date.getMinutes();
+var sec = date.getSeconds();
+time_el.innerHTML = hour+':'+min+':'+sec;
+
